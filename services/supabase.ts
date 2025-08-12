f517c4730c4acb316f2d37f959b927d12b699292
kryzkaik11 <rinamadness013@gmail.com>
2025-08-12 11:08:22 +0800

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,8 +1,10 @@
+/// <reference types="vite/client" />
+
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Supabase URL and Anon Key must be provided in environment variables.');
