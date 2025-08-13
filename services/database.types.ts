ae89beb7529eb4bd3458409b0e6bcef33d0ee45a
kryzkaik11 <rinamadness013@gmail.com>
2025-08-13 11:57:52 +0800

    Update database.types.ts

--- services/database.types.ts
+++ services/database.types.ts
@@ -1,12 +1,7 @@
 
 
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[];
+
+export type Json = any;
 
 export interface Database {
   public: {
